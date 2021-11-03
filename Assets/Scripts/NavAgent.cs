1b980feff04cfbd679dcd5c60ce743daa5ae045c
philip <pkahly@gmail.com>
2021-11-03 19:16:33 -0400

    Add player detection

--- Assets/Scripts/NavAgent.cs
+++ Assets/Scripts/NavAgent.cs
@@ -7,17 +7,24 @@ using UnityEngine.AI;
 public class NavAgent : MonoBehaviour
 {
     System.Random rand = new System.Random();
-    NavMeshAgent agent;
+    private NavMeshAgent agent;
+    private Transform player;
+    private Light spotLight;
 
     public Transform pathHolder;
-    public float viewDistance = 20;
+    public float viewDistance = 50;
+    public float viewAngle = 70;
 
+    private LayerMask obstacleMask;
     private int pathIndex = 0;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
+        spotLight = GetComponentInChildren<Light>();
 
+        obstacleMask = LayerMask.GetMask("Obstacle");
 
         Vector3[] path = new Vector3[pathHolder.childCount];
         for (int i = 0; i < path.Length; i++)
@@ -33,6 +40,16 @@ public class NavAgent : MonoBehaviour
     {
         while (true)
         {
+            // Look for player
+            if (CanSeePlayer())
+            {
+                spotLight.color = Color.red;
+            }
+            else
+            {
+                spotLight.color = Color.green;
+            }
+
             // Check if we've reached the destination
             if (agent.velocity.sqrMagnitude <= 0.1)
             {
@@ -44,11 +61,51 @@ public class NavAgent : MonoBehaviour
                 agent.SetDestination(path[pathIndex]);
 
                 pathIndex++;
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(0.5f);
             }
 
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.1f);
         }
+    }
+
+    bool CanSeePlayer()
+    {
+        // Get View distance from visibility level
+        //float viewDistance = visibilityLevelToViewDistanceMap[playerStats.GetVisibility()];
+        float viewDistance = 15;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Within View Distance
+        if (distanceToPlayer > viewDistance)
+        {
+            return false;
+        }
+
+        // Line of Sight not blocked by obstacles
+        if (Physics.Linecast(transform.position, player.position, obstacleMask))
+        {
+            return false;
+        }
+
+        // Within View Angle
+        Vector3 dirToPlayer = (player.position - transform.position).normalized;
+        float angleToPlayer = Math.Abs(Vector3.Angle(transform.forward, dirToPlayer));
+        if (angleToPlayer > viewAngle)
+        {
+            return false;
+        }
+
+        /*
+        // Line of Sight not blocked by heavy cover
+        // Treat this the same as the lowest visibility for this time of day
+        float lowestViewDistance = visibilityLevelToViewDistanceMap[playerStats.GetObscuredVisibility()];
+        if (Physics.Linecast(transform.position, player.position, heavyCoverMask) && distanceToPlayer > lowestViewDistance)
+        {
+            return false;
+        }
+        */
+
+        return true;
     }
 
     /*
